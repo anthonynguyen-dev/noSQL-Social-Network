d8aabb298631b55d4325c06899ac55374b7b768f
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-02-24 15:44:19 -0600

    added to thoughts model

---
+++ models/thoughts.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+const { schema } = require("./user");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
