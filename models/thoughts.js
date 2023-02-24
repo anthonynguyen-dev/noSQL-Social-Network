a702e98e2ab8dd59d39c162f71d21d64e98ef803
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-02-24 15:51:43 -0600

    finished thoughts model

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,15 +1,35 @@
 const { Schema, model } = require("mongoose");
-const { schema } = require("./user");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCounts").get(function () {
+  return this.reactions.length;
 });
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
